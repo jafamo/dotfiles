936daf515e1e190a9ad43c737e3e9764c806883a
Javier <jafamo@gmail.com>
2017-04-17 21:14:45 +0200

    add function optimize

--- arch/cleanSystem.sh
+++ arch/cleanSystem.sh
@@ -1,12 +1,37 @@
 #!/bin/bash
 
-echo "### cleaning packages from: /var/cachepacman/pkg"
 
-sudo pacman -Sc
+function cleanCache(){
 
-echo "### cleaned cache system."
-echo
-echo
-echo "### Delete old packages "
-sudo pacman -Scc
+	echo "### cleaning packages from: /var/cachepacman/pkg"
+
+	sudo pacman -Sc
+
+	echo "### cleaned cache system."
+	echo
+	echo
+	echo "### Delete old packages "
+	sudo pacman -Scc
+
+}
+
+function optimizeSystem(){
+	sudo pacman-optimize && sync
+	sudo updatedb
+	exit 0
+}
+
+read -p "Do you want clean Cache?  (s/n)  " option
+if [ "${option,,}" = "s" ]; then
+	cleanCache
+else
+	echo "no cache and exit"
+fi
+
+read -p "Do you want optimize your system (s/n) " optionOptimi
+if [ "${optionOptimi,,}" = "s" ]; then
+	optimizeSystem
+else
+	echo "no optimized your system"
+fi
 
