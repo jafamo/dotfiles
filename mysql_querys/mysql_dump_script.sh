3282150734bdf0673b5afcfe9ecae9951fab6111
Javier Farinos <jafamo@gmail.com>
2018-04-30 14:27:56 +0000

    Add new query in showSizeDB.sql

--- mysql_querys/mysql_dump_script.sh
+++ mysql_querys/mysql_dump_script.sh
@@ -1,21 +1,24 @@
-#!/bin/bash
-#----------------------------------------------------------
+#!/bin/sh
+
+#----------------------------------------------------
 # a simple mysql database backup script.
 # version 2, updated March 26, 2011.
-# copyright 2011 alvin alexander, http://alvinalexander.com
-#----------------------------------------------------------
+# copyright 2011 alvin alexander, http://devdaily.com
+#----------------------------------------------------
 # This work is licensed under a Creative Commons 
 # Attribution-ShareAlike 3.0 Unported License;
 # see http://creativecommons.org/licenses/by-sa/3.0/ 
 # for more information.
-#----------------------------------------------------------
+#----------------------------------------------------
+
 
 # (1) set up all the mysqldump variables
-FILE=minime.sql.`date +"%Y%m%d"`
+FOLDER=/dades/backup/
+FILE=${FOLDER}$1.`date +"%Y%m%d"`
 DBSERVER=127.0.0.1
-DATABASE=XXX
-USER=XXX
-PASS=XXX
+DATABASE=$1
+USER=$2
+PASS=$3
 
 # (2) in case you run this more than once a day, remove the previous version of the file
 unalias rm     2> /dev/null
@@ -28,7 +31,7 @@ rm ${FILE}.gz  2> /dev/null
 #mysqldump --opt --protocol=TCP --user=${USER} --password=${PASS} --host=${DBSERVER} ${DATABASE} > ${FILE}
 
 # use this command for a database server on localhost. add other options if need be.
-mysqldump --opt --user=${USER} --routines --password=${PASS} ${DATABASE} > ${FILE}
+mysqldump --opt --user=${USER} --password=${PASS} ${DATABASE} --add-drop-table > ${FILE}
 
 # (4) gzip the mysql database dump file
 gzip $FILE
@@ -36,3 +39,4 @@ gzip $FILE
 # (5) show the user the result
 echo "${FILE}.gz was created:"
 ls -l ${FILE}.gz
+
--- mysql_querys/showSizeDB.sql
+++ mysql_querys/showSizeDB.sql
@@ -9,3 +9,16 @@ SELECT
 FROM information_schema.TABLES
 GROUP BY table_schema;
 
+
+/** Esta te lo muestra con mas precision
+*/
+
+select
+    table_schema "Base de datos",
+    convert(sum(data_length+index_length)/1048576,decimal(6,2)) "SIZE (MB)"
+from
+    information_schema.tables
+where
+    table_schema!="information_schema"
+group by
+    table_schema;
