30d21c7448ea4757003b803130d809be4f9b863e
javier farinos <jafamo@gmail.com>
2018-07-10 15:50:56 +0000

    Add check directory database backup

--- mysql_querys/database_dump.sh
+++ mysql_querys/database_dump.sh
@@ -2,17 +2,14 @@
 
 #----------------------------------------------------------
 # a simple mysql database backup script.
-# version 2, updated March 26, 2011.
-# copyright 2011 alvin alexander, http://alvinalexander.com
+# version 2, updated July 10, 2018.
+# copyright 2018 Javier Farinos, http://jafamo.org
 #----------------------------------------------------------
 # This work is licensed under a Creative Commons 
 # Attribution-ShareAlike 3.0 Unported License;
 # see http://creativecommons.org/licenses/by-sa/3.0/ 
 # for more information.
 #----------------------------------------------------------
-echo "$1 es la base de datos"
-echo "$2 es el usuario"
-echo "$3 es el password "
 
 # (1) set up all the mysqldump variables
 FOLDER=/mnt/linux/backup/databases/
@@ -21,6 +18,12 @@ DBSERVER=127.0.0.1
 DATABASE=$1
 USER=$2
 PASS=$3
+
+if [ ! -d "$FOLDER$DATABASE" ]; then
+    mkdir $FOLDER$DATABASE;
+fi
+
+FILE=$FOLDER$DATABASE/$1.sql.`date +"%Y%m%d"`
 
 # (2) in case you run this more than once a day, remove the previous version of the file
 unalias rm     2> /dev/null
